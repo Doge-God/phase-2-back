c05c8ac85d7b4afdfe4a848104a2b5a4c0c70255
FZ <62817503+Doge-God@users.noreply.github.com>
2022-08-09 23:33:59 +1200

    Add project files.

---
+++ phase-2-back/personInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace phase_2_back
+{
+    public class personInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string ip { get; set; }
+        public string location { get; set; }
+    }
+}
