d012e3ad1eeede3657863e3999887be8718f013d
FZ <62817503+Doge-God@users.noreply.github.com>
2022-08-10 11:21:43 +1200

    Finalize documentation.

--- phase-2-back/Program.cs
+++ phase-2-back/Program.cs
@@ -18,19 +18,8 @@ builder.Services.AddSwaggerGen(options =>
     options.SwaggerDoc("v1", new OpenApiInfo
     {
         Version = "v1",
-        Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
-        }
+        Title = "Doge's Weird API",
+        Description = "An ASP.NET Core Web API for managing IP info and other stuff.",
     });
 
     // using System.Reflection;
